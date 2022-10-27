bbae610f92a4b588d591c679d808cb990c69b87e
Elliot662 <elliotmeltzer@hotmail.co.uk>
2022-10-27 12:45:24 +0100

    first commit

---
+++ code.js
@@ -0,0 +1,152 @@
+document.addEventListener('keyup', (event) => {
+    if (event.key == 'a') {
+        const audio1 = new Audio('./sounds/boom.wav');
+        audio1.loop = false;
+        audio1.play();
+    } else if (event.key == 'A') {
+        const audio1 = new Audio('./sounds/boom.wav');
+        audio1.loop = false;
+        audio1.play();
+    } else {}
+});
+document.getElementById("a").addEventListener("click", () => {
+    const audio1 = new Audio('./sounds/boom.wav');
+    audio1.loop = false;
+    audio1.play();
+})
+
+document.addEventListener('keyup', (event) => {
+    if (event.key == 's') {
+        const audio2 = new Audio('./sounds/clap.wav');
+        audio2.loop = false;
+        audio2.play();
+    } else if (event.key == 'S') {
+        const audio2 = new Audio('./sounds/clap.wav');
+        audio2.loop = false;
+        audio2.play();
+    } else {}
+});
+document.getElementById("s").addEventListener("click", () => {
+    const audio2 = new Audio('./sounds/clap.wav');
+    audio2.loop = false;
+    audio2.play();
+})
+
+document.addEventListener('keyup', (event) => {
+    if (event.key == 'd') {
+        const audio3 = new Audio('./sounds/hihat.wav');
+        audio3.loop = false;
+        audio3.play();
+    } else if (event.key == 'D') {
+        const audio3 = new Audio('./sounds/hihat.wav');
+        audio3.loop = false;
+        audio3.play();
+    } else {}
+});
+document.getElementById("d").addEventListener("click", () => {
+    const audio3 = new Audio('./sounds/hihat.wav');
+    audio3.loop = false;
+    audio3.play();
+})
+
+document.addEventListener('keyup', (event) => {
+    if (event.key == 'f') {
+        const audio4 = new Audio('./sounds/kick.wav');
+        audio4.loop = false;
+        audio4.play();
+    } else if (event.key == 'F') {
+        const audio4 = new Audio('./sounds/kick.wav');
+        audio4.loop = false;
+        audio4.play();
+    } else {}
+});
+document.getElementById("f").addEventListener("click", () => {
+    const audio4 = new Audio('./sounds/kick.wav');
+    audio4.loop = false;
+    audio4.play();
+})
+
+document.addEventListener('keyup', (event) => {
+    if (event.key == 'g') {
+        const audio5 = new Audio('./sounds/openhat.wav');
+        audio5.loop = false;
+        audio5.play();
+    } else if (event.key == 'G') {
+        const audio5 = new Audio('./sounds/openhat.wav');
+        audio5.loop = false;
+        audio5.play();
+    } else {}
+});
+document.getElementById("g").addEventListener("click", () => {
+    const audio5 = new Audio('./sounds/openhat.wav');
+    audio5.loop = false;
+    audio5.play();
+})
+
+document.addEventListener('keyup', (event) => {
+    if (event.key == 'h') {
+        const audio6 = new Audio('./sounds/ride.wav');
+        audio6.loop = false;
+        audio6.play();
+    } else if (event.key == 'H') {
+        const audio6 = new Audio('./sounds/ride.wav');
+        audio6.loop = false;
+        audio6.play();
+    } else {}
+});
+document.getElementById("h").addEventListener("click", () => {
+    const audio6 = new Audio('./sounds/ride.wav');
+    audio6.loop = false;
+    audio6.play();
+})
+
+document.addEventListener('keyup', (event) => {
+    if (event.key == 'j') {
+        const audio7 = new Audio('./sounds/snare.wav');
+        audio7.loop = false;
+        audio7.play();
+    } else if (event.key == 'J') {
+        const audio7 = new Audio('./sounds/snare.wav');
+        audio7.loop = false;
+        audio7.play();
+    } else {}
+});
+document.getElementById("j").addEventListener("click", () => {
+    const audio7 = new Audio('./sounds/snare.wav');
+    audio7.loop = false;
+    audio7.play();
+})
+
+document.addEventListener('keyup', (event) => {
+    if (event.key == 'k') {
+        const audio8 = new Audio('./sounds/tink.wav');
+        audio8.loop = false;
+        audio8.play();
+    } else if(event.key == 'K') {
+        const audio8 = new Audio('./sounds/tink.wav');
+        audio8.loop = false;
+        audio8.play();
+    } else {}
+});
+document.getElementById("k").addEventListener("click", () => {
+    const audio8 = new Audio('./sounds/tink.wav');
+    audio8.loop = false;
+    audio8.play();
+})
+
+document.addEventListener('keyup', (event) => {
+    if (event.key == 'l') {
+        const audio9 = new Audio('./sounds/tom.wav');
+        audio9.loop = false;
+        audio9.play();
+    } else if (event.key == 'L') {
+        const audio9 = new Audio('./sounds/tom.wav');
+        audio9.loop = false;
+        audio9.play();
+    } else {}
+});
+document.getElementById("l").addEventListener("click", () => {
+    const audio9 = new Audio('./sounds/tom.wav');
+    audio9.loop = false;
+    audio9.play();
+})
